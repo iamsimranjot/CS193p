4ef8e9982d2692bbb37e6ef1379c8575aeadd850
iamsimranjot <iamsimranjot@gmail.com>
2020-05-27 14:29:50 +0530

    Assignment 1 completed

--- Memorize/Memorize/EmojiMemoryGame.swift
+++ Memorize/Memorize/EmojiMemoryGame.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+import SwiftUI
+
+class EmojiMemoryGame {
+    
+    private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    private static let availableEmojis = ["👻", "🎃", "🕷", "🧛🏻", "🧟‍♂️", "🥶", "💩", "🌚", "🍆", "🍭", "🎈", "⚽️"]
+        
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis = availableEmojis.shuffled()
+        return MemoryGame<String>(numberOfPairsOfCards: Int.random(in: 2...5)) { pairIndex in
+            emojis[pairIndex]
+        }
+    }
+    
+    // MARK: - Access to the Model
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        model.cards
+    }
+    
+    // MARK: - Intent(s)
+    
+    func choose(card: MemoryGame<String>.Card) {
+        model.choose(card: card)
+    }
+}
